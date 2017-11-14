265cb5e17b4f1469db246cf5d245d95c6888a148
Michael Hays <michaelhays36@gmail.com>
2017-11-14 17:25:10 -0600

    Updated current submission interface

--- extension/src/popup/components/CurrentSubmission.vue
+++ extension/src/popup/components/CurrentSubmission.vue
@@ -5,8 +5,10 @@
     </div>
     <div v-if="currentSubmission">
       <p><strong>Project name:</strong> {{ currentSubmission.project.name }}</p>
-      <p><strong>Price:</strong> ${{ currentSubmission.price }}</p>
       <p><strong>Submission ID:</strong> {{ currentSubmission.id }}</p>
+      <p><strong>Time assigned:</strong> {{ currentSubmission.assigned_at.substring(11, 16) }} UTC</p>
+      <p><strong>Price:</strong> ${{ currentSubmission.price }}</p>
+      <p><a :href="'https://review.udacity.com/#!/submissions/' + String(currentSubmission.id)" target="_blank">Resume review</a></p>
     </div>
     <div v-else>
       <p>No currently assigned submissions.</p>
