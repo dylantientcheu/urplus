cdfb83cd2db23bf86e18d46caae0179b82e23eac
Michael Hays <michaelhays36@gmail.com>
2018-02-27 16:10:32 -0600

    Revert "Add currency conversion in popup"
    This reverts commit 130096fb9de1bcb6b6e6d9f38263dd23f5650663.

--- extension/src/popup/App.vue
+++ extension/src/popup/App.vue
@@ -1,28 +1,19 @@
 <template>
   <div id="app">
-    <h3 style="display: inline-block">Income</h3>
-    <select id="currency" v-model="currency">
-      <option>USD</option>
-      <option>EUR</option>
-      <option>JPY</option>
-      <option>GBP</option>
-      <option>AUD</option>
-      <option>CAD</option>
-      <option>INR</option>
-    </select><br>
-    <span><strong>Today:</strong> {{ convertCurrency(dailyUSDIncome) }} {{ currency }}</span><br>
-    <span><strong>This month:</strong> {{ convertCurrency(monthlyUSDIncome) }} {{ currency }}</span>
+    <h3>Income</h3>
+    <p><strong>Today:</strong> ${{ dailyIncome }}</p>
+    <p><strong>This month:</strong> ${{ monthlyIncome }}</p>
     <hr>
     <h3>First Active Review</h3>
     <div v-if="activeReview">
-      <span><strong>Project name:</strong> {{ activeReview.project.name }}</span><br>
-      <span><strong>Submission ID:</strong> {{ activeReview.id }}</span><br>
-      <span><strong>Assigned at:</strong> {{ (new Date(activeReview.assigned_at)).toLocaleTimeString() }}</span><br>
-      <span><strong>Price:</strong> ${{ activeReview.price }}</span><br>
-      <span><a :href="`https://review.udacity.com/#!/submissions/${activeReview.id}`" target="_blank">Resume review</a></span>
+      <p><strong>Project name:</strong> {{ activeReview.project.name }}</p>
+      <p><strong>Submission ID:</strong> {{ activeReview.id }}</p>
+      <p><strong>Assigned at:</strong> {{ (new Date(activeReview.assigned_at)).toLocaleTimeString() }}</p>
+      <p><strong>Price:</strong> ${{ activeReview.price }}</p>
+      <p><a :href="`https://review.udacity.com/#!/submissions/${activeReview.id}`" target="_blank">Resume review</a></p>
     </div>
     <div v-else>
-      <span>No currently assigned reviews.</span>
+      <p>No currently assigned reviews.</p>
     </div>
   </div>
 </template>
@@ -32,12 +23,8 @@ export default {
   name: 'app',
   data: () => ({
     activeReview: null,
-    currency: 'USD',
-    dailyUSDIncome: 0,
-    monthlyUSDIncome: 0,
-    rates: {
-      USD: 1,
-    },
+    dailyIncome: 0,
+    monthlyIncome: 0,
   }),
   methods: {
     load() {
@@ -49,14 +36,14 @@ export default {
       setTimeout(this.runRecurring, 10000);
     },
     updatePopup() {
-      chrome.storage.local.get('dailyUSDIncome', (data) => {
-        if ('dailyUSDIncome' in data) {
-          this.dailyUSDIncome = data.dailyUSDIncome;
+      chrome.storage.local.get('dailyIncome', (data) => {
+        if ('dailyIncome' in data) {
+          this.dailyIncome = data.dailyIncome;
         }
       });
-      chrome.storage.local.get('monthlyUSDIncome', (data) => {
-        if ('monthlyUSDIncome' in data) {
-          this.monthlyUSDIncome = data.monthlyUSDIncome;
+      chrome.storage.local.get('monthlyIncome', (data) => {
+        if ('monthlyIncome' in data) {
+          this.monthlyIncome = data.monthlyIncome;
         }
       });
       chrome.storage.local.get('activeReview', (data) => {
@@ -66,14 +53,6 @@ export default {
       });
       setTimeout(this.updatePopup, 100);
     },
-    convertCurrency(value) {
-      return (value * this.rates[this.currency]).toFixed(2);
-    },
-  },
-  beforeCreate() {
-    fetch('https://api.fixer.io/latest?base=USD')
-      .then(response => response.json())
-      .then((json) => { this.rates = Object.assign({}, this.rates, json.rates); });
   },
   mounted() {
     this.load();
@@ -86,7 +65,6 @@ export default {
   background: rgb(242, 242, 242);
   font-size: 16px;
   height: 400px;
-  line-height: 1.5;
   padding: 10px;
   width: 400px;
 }
