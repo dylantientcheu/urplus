25fb7de82142dea4870b93276dae3b0bc0ea1ac0
Dylan Tientcheu <dylanmambou17@gmail.com>
2018-07-23 22:43:09 +0100

    updates relative to review api changes

--- extension/src/popup/App.vue
+++ extension/src/popup/App.vue
@@ -1,65 +1,12 @@
 <template>
   <div id="app">
-    <income-box class="box"/>
-    <review-box class="box"/>
+    <h2>UR Plus Extension</h2>
   </div>
 </template>
 
 <script>
-import Income from './components/Income';
-import Review from './components/Review';
-
 export default {
   name: 'app',
-  components: {
-    'income-box': Income,
-    'review-box': Review,
-  },
-  methods: {
-    runRecurring() {
-      chrome.runtime.sendMessage({ func: 'runRecurring' });
-      setTimeout(this.runRecurring, 10000);
-    },
-    updatePopup() {
-      chrome.storage.local.get('dailyUSDIncome', (data) => {
-        if ('dailyUSDIncome' in data) {
-          this.$store.commit('updateProperty', {
-            property: 'dailyUSDIncome',
-            value: data.dailyUSDIncome,
-          });
-        }
-      });
-      chrome.storage.local.get('monthlyUSDIncome', (data) => {
-        if ('monthlyUSDIncome' in data) {
-          this.$store.commit('updateProperty', {
-            property: 'monthlyUSDIncome',
-            value: data.monthlyUSDIncome,
-          });
-          this.monthlyUSDIncome = data.monthlyUSDIncome;
-        }
-      });
-      chrome.storage.local.get('activeReview', (data) => {
-        if ('activeReview' in data) {
-          this.$store.commit('updateProperty', {
-            property: 'activeReview',
-            value: data.activeReview,
-          });
-        }
-      });
-      setTimeout(this.updatePopup, 100);
-    },
-  },
-  beforeCreate() {
-    fetch('https://api.fixer.io/latest?base=USD')
-      .then(response => response.json())
-      .then((json) => {
-        this.$store.commit('updateRates', { newRates: json.rates });
-      });
-  },
-  mounted() {
-    this.runRecurring();
-    this.updatePopup();
-  },
 };
 </script>
 
@@ -68,26 +15,9 @@ body {
   margin: 0 !important;
 
   #app {
-    background: rgb(242, 242, 242);
     font-size: 16px;
     height: auto;
-    width: 500px;
-
-    .box {
-      background-color: white;
-      border-radius: 10px;
-      box-shadow: 2px 4px 4px -4px rgba(0, 0, 0, 0.68);
-      padding: 10px;
-
-      .box-title {
-        display: inline-block;
-        font-size: 1.25rem;
-        font-weight: 500;
-        letter-spacing: 2px;
-        margin-top: 0px;
-        text-transform: uppercase;
-      }
+    padding: 15px;
     }
   }
-}
 </style>
--- extension/src/popup/store/mutations.js
+++
@@ -1,7 +0,0 @@
-export function updateProperty(state, { property, value }) {
-  state[property] = value;
-}
-
-export function updateRates(state, { newRates }) {
-  state.rates = Object.assign({}, state.rates, newRates);
-}
